b2e8e6f8db56fc9d1b746c8d5161f29d0dad9cd0
s.garmaev <garmsayan@gmail.com>
2018-05-31 14:02:41 +0300

    Allure2

--- src/test/java/com/test/util/RestApiUtils.java
+++ src/test/java/com/test/util/RestApiUtils.java
@@ -4,7 +4,7 @@ public class RestApiUtils {
 
     public Integer getGoodsRemains(String goodsId) {
         HttpMethods httpMethods = new HttpMethods();
-        httpMethods.get(EndPoint.GET_GOODS_REMAINS_BY_ID + "/?conditions=goods_id%20IN%20(" + goodsId + ")",200).
+        httpMethods.get(EndPoint.GET_GOODS_REMAINS_BY_ID + "/?conditions=goods_id IN (" + goodsId + ")",200).
                 then();
         return 1;
     }
